78f6c63bfb6742ca0c021cefeabf7973d190affa
James Tapsell <email@jrtapsell.co.uk>
2018-05-05 16:56:13 +0100

    Moving to Jest

--- __tests__/common.js
+++ __tests__/common.js
@@ -1,10 +1,11 @@
-const utils = require("./utils/utils");
+const utils = require("./utils");
 
 const data = [
     {"domain": "jrtapsell.co.uk", "date": "2022-12-03T00:00:00.000Z"},
     {"domain": "google.com", "date": "2020-09-14T04:00:00Z"},
     {"domain": "facebook.com", "date": "2025-03-30T04:00:00Z"},
-    {"domain": "nxdomain.nxdomain", "error": "Cannot parse the whois data"}
+    {"domain": "nxdomain.nxdomain", "error": "Cannot parse the whois data"},
+    {"domain": undefined, "error": "Domain cannot be undefined"}
 ];
 
 module.exports = utils.forEachDomain(data);
