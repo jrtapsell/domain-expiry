999b7558057e56ab384273fbacf75ece32548ee9
James Tapsell <email@jrtapsell.co.uk>
2018-05-05 17:49:45 +0100

    Cleanup

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -2,11 +2,11 @@ const index = require("../index");
 
 module.exports = {
     forEachDomain(data) {
-        data.forEach(item => {
+        data.forEach((item) => {
             it("Testing: " + item.domain, async () => {
                 try {
                     const expiry = await index.getExpiry(item.domain);
-                    expect(expiry.getTime()).toEqual(new Date(item.date).getTime())
+                    expect(expiry.getTime()).toEqual(new Date(item.date).getTime());
                 } catch (err) {
                     expect(typeof err.message).toEqual("string");
                     expect(err.message).toEqual(item.error);
@@ -17,4 +17,4 @@ module.exports = {
     }
 };
 
-it("Utility file", async ()=>{});
+it("Utility file", async () => {});
