f03ef26f7cca936bf90ef0a6680300dae78e8da6
James Tapsell <email@jrtapsell.co.uk>
2018-11-01 23:03:45 +0000

    Updated facebook's expiry date

--- __tests__/common.js
+++ __tests__/common.js
@@ -3,7 +3,7 @@ const utils = require("./utils");
 const data = [
     {"domain": "jrtapsell.co.uk", "date": "2022-12-03T00:00:00.000Z"},
     {"domain": "google.com", "date": "2020-09-14T04:00:00Z"},
-    {"domain": "facebook.com", "date": "2025-03-30T04:00:00Z"},
+    {"domain": "facebook.com", "date": "2028-03-30T04:00:00Z"},
     {"domain": "nxdomain.nxdomain", "error": "Cannot parse the whois data"},
     {"domain": undefined, "error": "Domain cannot be undefined"}
 ];
